49fe37f2f2f0e5ecf963d28382b9b70dfb2c9d2b
Ryan Bauroth <25bauroth@da.org>
2023-02-01 11:12:39 -0500

    Added Interface Methods to SubWordFinder.java

--- src/WordFinder.java
+++ src/WordFinder.java
@@ -1,13 +1,14 @@
+/**
+ * SubWordInterface is a simple interface that forces students
+ * to implement and comply with public method specifications.
+ * @author Julian Cochran
+ * @version December 7, 2014
+ */
+
 import java.util.ArrayList;
 
-/** TODO add javadoc
- * @Author Ryfi
- * @Version 02.09.23
- */
-public class SubWordFinder implements WordFinder{
-    public static void main(String[] args) {
 
-    }
+public interface WordFinder {
     /**
      * Populates the dictionary from the text file contents
      * The dictionary object should contain 26 buckets, each
@@ -15,10 +16,8 @@ public class SubWordFinder implements WordFinder{
      * The String objects in the buckets are sorted A-Z because
      * of the nature of the text file words.txt
      */
-    @Override
-    public void populateDictionary() {
+    void populateDictionary();
 
-    }
     /**
      * Retrieve all SubWord objects from the dictionary.
      * A SubWord is defined as a word that can be split into two
@@ -28,18 +27,14 @@ public class SubWordFinder implements WordFinder{
      * "base" and "ball" (no unused characters)
      * To do this, you must look through every word in the dictionary
      * to see if it is a SubWord object
-     *
      * @return An ArrayList containing the SubWord objects
      * pulled from the file words.txt
      */
-    @Override
-    public ArrayList<SubWord> getSubWords() {
-        return null;
-    }
+    ArrayList<SubWord> getSubWords();
+
     /**
      * Look through the entire dictionary object to see if
      * word exists in dictionary
-     *
      * @param word The item to be searched for in dictionary
      * @return true if word is in dictionary, false otherwise
      * NOTE: EFFICIENCY O(log N) vs O(N) IS A BIG DEAL HERE!!!
@@ -47,8 +42,5 @@ public class SubWordFinder implements WordFinder{
      * YOUR OWN DEFINITION of a binary search in order to receive
      * the credit as specified on the grading rubric.
      */
-    @Override
-    public boolean inDictionary(String word) {
-        return false;
-    }
+    boolean inDictionary(String word);
 }
